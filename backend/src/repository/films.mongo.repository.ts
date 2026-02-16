7b77398edc8dd1bee22dc5127e4da8d76d6c599a
SavDmiPracticum <155889935+SavDmiPracticum@users.noreply.github.com>
2026-02-16 14:41:38 +0300

    ПР16 переход на PG (#2)
    * ПР16 переход на PG
    
    * PR16 dependencies
    
    * PP16 fixing
    
    * PR16 tests
    
    * PR16 test DB films!!
    
    * PR16 fix
    
    * PR16 fix
    
    * PR16
    
    * PR18=6 entity
    
    * PR16 schedule

--- backend/src/repository/films.mongo.repository.ts
+++ backend/src/repository/films.mongo.repository.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Film } from './films.schema';
+import { FilmsRepository } from './films.type';
 
 @Injectable()
-export class FilmsRepository {
+export class FilmsMongoRepository implements FilmsRepository {
   constructor(@InjectModel('film') private filmModel: Model<Film>) {}
 
   async getFilms(): Promise<Film[]> {
